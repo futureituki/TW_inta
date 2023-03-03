56b02e198f178a03c148d2416a36df0e1d84e402
mattya3 <m.tkm1217@ezweb.ne.jp>
2023-03-03 17:32:05 +0900

    ..

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,6 +16,15 @@ app.listen(80, () => {
 app.get("/helth-check", async (req, res) => {
   const handler = await DBHandler.init()
   const findResult = await handler.collection("test").find({}).toArray()
+  console.log("test")
   console.log(findResult)
   res.send(JSON.stringify({ test: findResult }))
+})
+app.get("/postArticle", async (req, res) => {
+  console.log("post")
+  res.send(JSON.stringify("ok"))
+})
+app.get("/login", async (req, res) => {
+  console.log("login")
+  res.send(JSON.stringify("ok"))
 })
