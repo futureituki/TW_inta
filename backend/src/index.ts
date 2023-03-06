cda99a11df9a2ddce391107b4730b7dc2a15e8fb
mattya3 <m.tkm1217@ezweb.ne.jp>
2023-03-06 17:44:21 +0900

    server: delete func

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { query } from "express"
 import { DBHandler } from "./infrastructure/database"
 
 const app = express()
@@ -13,32 +13,67 @@ app.use((req, res, next) => {
 app.listen(80, () => {
   console.log("Start on port 80.")
 })
-app.get("/helth-check", async (req, res) => {
-  const handler = await DBHandler.init()
-  const findResult = await handler.collection("test").find({}).toArray()
-  console.log("test")
-  console.log(findResult)
-  res.send(JSON.stringify({ test: findResult }))
-})
 app.post("/Articles", async (req, res) => {//投稿
-  console.log("test")
-  res.send(JSON.stringify("ok"))
+  console.log("new")
+  var queryList = req.url.slice(11, req.url.length).split('&')
+  var id = queryList[0].split('=')[1]
+  var type = queryList[1].split('=')[1]
+  for(var i = 2; i < queryList.length; i++){
+    var splited = queryList[i].split('=')
+    switch(type){
+      case "競泳":
+        switch(splited[0]){
+          case "種類": var genre = splited[1]; break;
+          case "タイム": var time = splited[1]; break;
+          case "映像": var camera = splited[1]; break;
+          case "水中": var water = splited[1]; break;
+          case "シーン": var scene = splited[1]; break;
+        }
+        break;
+      case "グッズ":
+        switch(splited[0]){
+          case "ゴーグル": var goggles = splited[1]; break;
+          case "ビート板": var gickBoard = splited[1]; break;
+        }
+        break;
+      case "オフトレ":
+        switch(splited[0]){
+          case "トライランド": var triland = splited[1]; break;
+          case "ウェイト": var weight = splited[1]; break;
+          case "食事": var eat = splited[1]; break;
+        }
+        break;
+    }
+  }
+  const handler = await DBHandler.init()
+  var count = 5/*検索し、投稿者のarcicleNumを獲得する*/
+  count++
+  handler.collection('user').updateOne({user: id}, {arcicleNum: count})
+  handler.collection('user').insertOne({}) /*投稿する*/
+  res.send(JSON.stringify("true"))
 })
 app.put("/Articles", async (req, res) => {//更新
-  console.log("post")
+  console.log("update")
   res.send(JSON.stringify("ok"))
 })
 app.get("/Articles", async (req, res) => {//取得
-  console.log("post")
-  res.send(JSON.stringify("ok"))
+  console.log("get")
+  const handler = await DBHandler.init()
+  const answer = handler.collection('article').find().toArray() /*検索文にて探す*/
+  res.send(JSON.stringify(answer))
 })
 app.delete("/Articles", async (req, res) => {//削除
-  res.send(JSON.stringify("bye"))
+  res.send(JSON.stringify("delate"))
+  var queryList = req.url.slice(11, req.url.length).split('&')
+  var userId = queryList[0].split('=')[1]
+  var articleId = queryList[1].split('=')[1]
+  const handler = await DBHandler.init()
+  handler.collection('article').deleteOne({user: userId, article: articleId})
+  res.send(JSON.stringify("ok"))
 })
-app.get("/login", async (req, res) => {
-  console.log("login")
-  var queryList = req.url.slice(8, req.url.length).split('&')
-  if(queryList.length < 0) res.send(JSON.stringify(false))
+app.post("/User", async (req, res) => {//新規登録 post
+  console.log("register")
+  var queryList = req.url.slice(7, req.url.length).split('&')
   var id, pass
   for(var i = 0; i < queryList.length; i++){
     var splited = queryList[i].split('=')
@@ -48,6 +83,24 @@ app.get("/login", async (req, res) => {
     }
   }
   const handler = await DBHandler.init()
-  const findResult = await handler.collection('test').insertOne({user: id, pwd: pass})
-  res.send(JSON.stringify(findResult))
+  const findResult = await handler.collection('user').find({user: id}).count()
+  var flag = (findResult == 0)
+  if(flag) await handler.collection('user').insertOne({user: id, pwd: pass, articleCount: 0})
+  res.send(JSON.stringify(flag))
+})
+app.put("/User", async (req, res) => {//ログイン put
+  console.log("login")
+  var queryList = req.url.slice(7, req.url.length).split('&')
+  var id, pass
+  for(var i = 0; i < queryList.length; i++){
+    var splited = queryList[i].split('=')
+    switch(splited[0]){
+      case "id": id = splited[1]; break
+      case "pass": pass = splited[1]; break
+    }
+  }
+  const handler = await DBHandler.init()
+  const findResult = await handler.collection('user').find({user: id, pwd: pass}).count()
+  var flag = (findResult == 1)
+  res.send(JSON.stringify(flag))
 })
